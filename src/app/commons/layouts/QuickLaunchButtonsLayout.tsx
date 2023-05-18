1e70b256edb5440ff51e444133a0a392ef3161b1
Mathias Betancurt <matt@betancurt.dev>
2023-05-18 19:22:20 +0200

    chore: add reusable quick launch buttons layout

---
+++ src/app/commons/layouts/QuickLaunchButtonsLayout.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react';
+import styles from './QuickLaunchButtonsLayout.module.scss';
+import { useTranslation } from 'react-i18next';
+import { Icon, IconName } from '@blueprintjs/core';
+import { useHistory } from 'react-router-dom';
+import { useAuthIsAdmin } from '../../modules/auth/store';
+
+export interface QuickLaunchButton {
+	label: string;
+	icon: IconName;
+	path: string; // Relative to / (root)
+	isAdmin: boolean;
+}
+interface QuickLaunchButtonsProps {
+	title: string;
+	buttons: QuickLaunchButton[];
+}
+
+const QuickLaunchButtonsLayout: FC<QuickLaunchButtonsProps> = ({ title, buttons }) => {
+	const { t } = useTranslation();
+	const history = useHistory();
+	const isAdmin = useAuthIsAdmin();
+	return (
+		<div className={styles.horizontal_usecases}>
+			<div className={styles.title}>{t(title) || title}</div>
+			{buttons.map((button, index) => {
+				if (button.isAdmin && isAdmin) {
+					return (
+						<div
+							key={button.label}
+							className={styles.item}
+							onClick={() => history.push(button.path)}
+						>
+							<div>
+								<Icon icon={button.icon} size={64} />
+							</div>
+							<div>{t(button.label) || button.label}</div>
+						</div>
+					);
+				} else {
+					return null;
+				}
+			})}
+		</div>
+	);
+};
+
+export default QuickLaunchButtonsLayout;
---
+++ src/app/commons/screens/Home.001.tsx
@@ -0,0 +1,126 @@
+import { Spinner, SpinnerSize } from '@blueprintjs/core';
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
+import PButton from '@pcomponents/PButton';
+import DashboardLayout from '../layouts/DashboardLayout';
+import { useQueryOperationsCounts } from '../../modules/core_service/operation/hooks';
+import { useOperationStore } from '../../modules/core_service/operation/store';
+import { shallow } from 'zustand/shallow';
+import styles from './Home.module.scss';
+import { getCSSVariable } from '../../utils';
+
+const StatNumber = ({ colorDiffThan0, stat }: { colorDiffThan0: string; stat: number }) => {
+	return (
+		<>
+			{stat >= 0 && (
+				<p
+					style={{
+						color: stat > 0 ? colorDiffThan0 : undefined
+					}}
+				>
+					{stat}
+				</p>
+			)}
+			{stat < 0 && <Spinner size={SpinnerSize.SMALL} />}
+		</>
+	);
+};
+
+const Home = () => {
+	const history = useHistory();
+	const { t } = useTranslation();
+
+	const {
+		setFilterProposed,
+		setFilterPending,
+		setFilterAccepted,
+		setFilterActivated,
+		setFilterRogue,
+		setFilterNotAccepted,
+		setFilterClosed
+	} = useOperationStore(
+		(state) => ({
+			setFilterProposed: state.setFilterProposed,
+			setFilterPending: state.setFilterPending,
+			setFilterAccepted: state.setFilterAccepted,
+			setFilterActivated: state.setFilterActivated,
+			setFilterRogue: state.setFilterRogue,
+			setFilterNotAccepted: state.setFilterNotAccepted,
+			setFilterClosed: state.setFilterClosed
+		}),
+		shallow
+	);
+
+	const setUniqueFilterAndVisitOperationsHub = (type: string) => {
+		setFilterProposed(false);
+		setFilterPending(type === 'PENDING');
+		setFilterAccepted(type === 'ACCEPTED');
+		setFilterActivated(type === 'ACTIVATED');
+		setFilterRogue(type === 'ROGUE');
+		setFilterNotAccepted(type === 'NOT_ACCEPTED');
+		setFilterClosed(type === 'CLOSED');
+		history.push('/operations');
+	};
+
+	const { countPending, countActivated, countRogue } = useQueryOperationsCounts();
+	/*const countVehiclesPending = useQueryPendingVehiclesCount();
+	const countUsersPending = useQueryPendingUsersCount();*/
+
+	// get css var --yokohama-yamate-500
+	const colors = {
+		success: getCSSVariable('yokohama-yamate-500'),
+		warning: getCSSVariable('yokohama-kannai-500'),
+		danger: getCSSVariable('yokohama-ramen-500')
+	};
+
+	return (
+		<DashboardLayout isLoading={false}>
+			<div className={styles.home}>
+				{/*!isPilot && (
+						<div>
+							<StatNumber stat={countUsersPending} colorDiffThan0={colors.primary} />
+							<div>{t('Users pending authorization')}</div>
+							<PButton
+								icon={'eye-open'}
+								onClick={() => history.push('/users?unconfirmed=true')}
+							/>
+						</div>
+					)*/}
+				{/*<div>
+						<StatNumber stat={countVehiclesPending} colorDiffThan0={colors.primary} />
+						<div>{t('Vehicles pending authorization')}</div>
+						<PButton
+							icon={'eye-open'}
+							onClick={() => history.push('/vehicles?pending=true')}
+						/>
+					</div>*/}
+				<div>
+					<StatNumber stat={countPending} colorDiffThan0={colors.warning} />
+					<div>{t('Operation(s) in PENDING state')}</div>
+					<PButton
+						icon={'eye-open'}
+						onClick={() => setUniqueFilterAndVisitOperationsHub('PENDING')}
+					/>
+				</div>
+				<div>
+					<StatNumber stat={countActivated} colorDiffThan0={colors.success} />
+					<div>{t('Operation(s) in ACTIVATED state')}</div>
+					<PButton
+						icon={'eye-open'}
+						onClick={() => setUniqueFilterAndVisitOperationsHub('ACTIVATED')}
+					/>
+				</div>
+				<div>
+					<StatNumber stat={countRogue} colorDiffThan0={colors.danger} />
+					<div>{t('Operation(s) in ROGUE state')}</div>
+					<PButton
+						icon={'eye-open'}
+						onClick={() => setUniqueFilterAndVisitOperationsHub('ROGUE')}
+					/>
+				</div>
+			</div>
+		</DashboardLayout>
+	);
+};
+
+export default Home;
