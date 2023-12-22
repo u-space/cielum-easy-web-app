3acf39a572a255df6480b9edfbc538fca3b54e17
Mathias Betancurt <matt@betancurt.dev>
2023-12-22 19:31:00 +0100

    feat: filter by creation date and starting time for flight requests

--- src/app/modules/flight_request_service/flight_request/components/FlightRequestSearchTools.tsx
+++ src/app/modules/flight_request_service/flight_request/components/FlightRequestSearchTools.tsx
@@ -3,13 +3,29 @@ import { useFlightRequestStore } from '../store';
 import FilterAndOrderSearchTools from '../../../../commons/components/hubs/FilterAndOrderSearchTools';
 import GridCheckboxes from '../../../../commons/layouts/dashboard/menu/GridCheckboxes';
 import CardGroup from '../../../../commons/layouts/dashboard/menu/CardGroup';
+import PDateInput from '@pcomponents/PDateInput';
+import { useTranslation } from 'react-i18next';
+import PButton from '@pcomponents/PButton';
+
+function dateToYYYYMMDD(date: Date) {
+	return [
+		date.getFullYear(),
+		('0' + (date.getMonth() + 1)).slice(-2),
+		('0' + date.getDate()).slice(-2)
+	].join('-');
+}
 
 const FlightRequestSearchTools: FC = () => {
+	const { t } = useTranslation(['ui', 'glossary']);
 	const store = useFlightRequestStore((state) => ({
 		filterShowPaid: state.filterShowPaid,
 		setFilterShowPaid: state.setFilterShowPaid,
 		filterShowNotPaid: state.filterShowNotPaid,
-		setFilterShowNotPaid: state.setFilterShowNotPaid
+		setFilterShowNotPaid: state.setFilterShowNotPaid,
+		filterProperty: state.filterProperty,
+		filterMatchingText: state.filterMatchingText,
+		setFilterProperty: state.setFilterProperty,
+		setFilterByText: state.setFilterByText
 	}));
 	const gridItems = [
 		{
@@ -28,10 +44,49 @@ const FlightRequestSearchTools: FC = () => {
 		<>
 			<FilterAndOrderSearchTools
 				useStore={useFlightRequestStore}
-				entityName={'flightRequest'}
-				searchableProps={['id', 'name']}
-				orderableProps={['createdAt', 'name']}
+				entityName={'flight-request'}
+				searchableProps={['id', 'name', 'starting_time', 'createdAt']}
+				orderableProps={['createdAt', 'name', 'starting_time']}
 			/>
+			<CardGroup header="Filter by date">
+				{store.filterProperty === 'starting_time' && (
+					<PDateInput
+						id="starting_time"
+						label={t('glossary:flightRequest.starting_time')}
+						defaultValue={new Date(store.filterMatchingText || new Date().getTime())}
+						onChange={(date) => store.setFilterByText(dateToYYYYMMDD(date))}
+					/>
+				)}
+				{store.filterProperty !== 'starting_time' && (
+					<PButton
+						onClick={() => {
+							store.setFilterProperty('starting_time');
+							store.setFilterByText(dateToYYYYMMDD(new Date()));
+						}}
+					>
+						{t('Filter by starting time')}
+					</PButton>
+				)}
+				{store.filterProperty === 'createdAt' && (
+					<PDateInput
+						id="createdAt"
+						label={t('glossary:flightRequest.createdAt')}
+						defaultValue={new Date(store.filterMatchingText || new Date().getTime())}
+						onChange={(date) => store.setFilterByText(dateToYYYYMMDD(date))}
+					/>
+				)}
+				{store.filterProperty !== 'createdAt' && (
+					<PButton
+						onClick={() => {
+							store.setFilterProperty('createdAt');
+							store.setFilterByText(dateToYYYYMMDD(new Date()));
+						}}
+					>
+						{t('Filter by creation date')}
+					</PButton>
+				)}
+			</CardGroup>
+
 			<CardGroup header="Filter by state">
 				<GridCheckboxes gridItems={gridItems} />
 			</CardGroup>
