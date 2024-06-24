d2806dd7d129bc33cc1998ed7e49fa75a6118414
emi <emialonzo@gmail.com>
2024-06-24 13:57:19 -0300

    reduce altitude multiplier

--- tokyo/src/converters/util.ts
+++ tokyo/src/converters/util.ts
@@ -1,4 +1,4 @@
-export const ELEVATION_MULTIPLIER = 5;
+export const ELEVATION_MULTIPLIER = 1;
 export function calculateLocationWithElevationMultiplier(coordinates: number[]) {
 	return [coordinates[0], coordinates[1], coordinates[2] * ELEVATION_MULTIPLIER];
 }
